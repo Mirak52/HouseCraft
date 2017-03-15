25a3762de814e102432da248692413df2ac8fe66
bastlma14 <bastlma14@DST20709>
2017-03-15 10:59:20 +0100

    Hospoda

--- Draci-doupe/Tavern.xaml.cs
+++ Draci-doupe/Tavern.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SQLite;
+using Draci_doupe.Tridy;
+using Draci_doupe.Třídy;
 
 namespace Draci_doupe
 {
@@ -21,8 +24,93 @@ namespace Draci_doupe
     {
         public Tavern()
         {
+            Inventory Skills = new Inventory();
+            Skills.Money = 500;
+            Database2.SaveItemAsync(Skills);
             InitializeComponent();
-            gsdfasdfasdfas
+            Money(0);
+
+        }
+        public void Money(int command)
+        {
+            var itemsFromDb = Database2.QueryGet().Result;
+            foreach (var osoby in itemsFromDb)
+            {
+                gold.Content = osoby.Money;
+                
+
+                if(command == 1)
+                {
+                    gold.Content = osoby.Money - 20;
+                    Inventory Skills = new Inventory();
+                    Skills.Money = osoby.Money - 20;
+                    Database2.SaveItemAsync(Skills);
+                    command = 0;
+                }
+                if(osoby.Money <= 20){Beer.Visibility = Visibility.Hidden;}
+            }
+        }
+        private void gossip_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void quest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Beer_Click(object sender, RoutedEventArgs e)
+        {
+            Money(1); //přidat 
+        }
+        private void City_Click(object sender, RoutedEventArgs e)
+        {
+            City customization = new City();
+            customization.Show();
+            this.Close();
+        }
+
+
+
+        public static EnemiesDatabase _database;
+        public static OsobyDatabase _database1;
+        public static InventoryDatabase _database2;
+        public static EnemiesDatabase Database
+        {
+            get
+            {
+                if (_database == null)
+                {
+                    var fileHelper = new Filehelper();
+                    _database = new EnemiesDatabase(fileHelper.GetLocalFilePath("TodoSQLite.db3"));
+                }
+                return _database;
+            }
+        }
+        public static OsobyDatabase Database1
+        {
+            get
+            {
+                if (_database1 == null)
+                {
+                    var fileHelper = new Filehelper();
+                    _database1 = new OsobyDatabase(fileHelper.GetLocalFilePath("TodoSQLite.db3"));
+                }
+                return _database1;
+            }
+        }
+        public static InventoryDatabase Database2
+        {
+            get
+            {
+                if (_database1 == null)
+                {
+                    var fileHelper = new Filehelper();
+                    _database2 = new InventoryDatabase(fileHelper.GetLocalFilePath("TodoSQLite.db3"));
+                }
+                return _database2;
+            }
         }
     }
 }
